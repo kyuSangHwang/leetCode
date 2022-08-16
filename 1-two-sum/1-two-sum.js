567780afd85fdae1d6eedf76d71a0325396b0390
DopeKS <kyusang.dev@gmail.com>
2022-08-16 10:38:15 +0900

    Time: 99 ms (73.10%), Space: 43.6 MB (20.79%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,18 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-	let hash = {};
-	
-	for(let i = 0; i < nums.length; i++) {
-		const n = nums[i];
-        
-		if(hash[target - n] !== undefined) {
-			return [hash[target - n], i];
-		}
-        
-		hash[n] = i;
-        
-	}
+    let map = new Map();
     
-	return;
-}
+    for(let i = 0; i < nums.length; i ++) {
+        if(map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+	return [];
+};
